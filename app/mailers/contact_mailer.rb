3888760a4d4cf380e13d63cc4bc37b26e25c4e51
eyahm8 <mikael.rasimus@gmail.com>
2016-12-14 02:24:15 +0700

    Added mailer settings to environment

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'eyahm8@example.com'
+  default to: 'mikael.rasimus@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
